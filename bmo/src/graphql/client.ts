e82b81baa51eeeead344e36dcab3b3a6e298de0f
Pyro-Warrior-1884 <albertaugustine1884@gmail.com>
2025-12-09 22:46:54 +0530

    hid the backend link into env

--- bmo/src/graphql/client.ts
+++ bmo/src/graphql/client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client/core";
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: "http://localhost:4000/graphql",
+    uri: import.meta.env.VITE_GRAPHQL_URL,
   }),
   cache: new InMemoryCache(),
 });
